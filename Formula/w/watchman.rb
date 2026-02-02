5723a0326bad36945124127cf3ee25afe9a17e03
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-02 17:10:19 +0000

    watchman 2026.02.02.00

--- Formula/w/watchman.rb
+++ Formula/w/watchman.rb
@@ -3,10 +3,9 @@ class Watchman < Formula
 
   desc "Watch files and take action when they change"
   homepage "https://github.com/facebook/watchman"
-  url "https://github.com/facebook/watchman/archive/refs/tags/v2026.01.12.00.tar.gz"
-  sha256 "5b6be267c159356a77511545b0608b0dcbd1dfa4c6277b0a5385fc221e85392a"
+  url "https://github.com/facebook/watchman/archive/refs/tags/v2026.02.02.00.tar.gz"
+  sha256 "240ac7f07ff71fb3dd84dc4f0f01648ff9dc0a3b4d3757b2d287b6a5c3565aaa"
   license "MIT"
-  revision 1
   head "https://github.com/facebook/watchman.git", branch: "main"
 
   bottle do
