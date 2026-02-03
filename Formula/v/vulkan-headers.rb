29b4ea9a1ac39ff551804affaec3b218331482c0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-03 03:28:12 +0000

    vulkan-headers 1.4.341.0

--- Formula/v/vulkan-headers.rb
+++ Formula/v/vulkan-headers.rb
@@ -1,8 +1,8 @@
 class VulkanHeaders < Formula
   desc "Vulkan Header files and API registry"
   homepage "https://github.com/KhronosGroup/Vulkan-Headers"
-  url "https://github.com/KhronosGroup/Vulkan-Headers/archive/refs/tags/vulkan-sdk-1.4.335.0.tar.gz"
-  sha256 "269e95cc5138ea0a0d52fcb0ee19102add2560fedf5a43b1b5c17780c2775764"
+  url "https://github.com/KhronosGroup/Vulkan-Headers/archive/refs/tags/vulkan-sdk-1.4.341.0.tar.gz"
+  sha256 "d73bc5036b6556b741f6985ff600ca720308c5f2850e4a43ceb498bd3de069e7"
   license "Apache-2.0"
   head "https://github.com/KhronosGroup/Vulkan-Headers.git", branch: "main"
 
