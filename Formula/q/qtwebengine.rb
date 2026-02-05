4c005edcf8670d9017eb650ff41c8c11d944d489
Michael Cho <michael@michaelcho.dev>
2026-02-05 17:50:44 -0500

    qtwebengine 6.10.2

--- Formula/q/qtwebengine.rb
+++ Formula/q/qtwebengine.rb
@@ -3,10 +3,10 @@ class Qtwebengine < Formula
 
   desc "Provides functionality for rendering regions of dynamic web content"
   homepage "https://www.qt.io/"
-  url "https://download.qt.io/official_releases/qt/6.10/6.10.1/submodules/qtwebengine-everywhere-src-6.10.1.tar.xz"
-  mirror "https://qt.mirror.constant.com/archive/qt/6.10/6.10.1/submodules/qtwebengine-everywhere-src-6.10.1.tar.xz"
-  mirror "https://mirrors.ukfast.co.uk/sites/qt.io/archive/qt/6.10/6.10.1/submodules/qtwebengine-everywhere-src-6.10.1.tar.xz"
-  sha256 "77b5ea6186a0429a6b8e656faedd5cd3e8019d33856ee59637698ab578ead1e3"
+  url "https://download.qt.io/official_releases/qt/6.10/6.10.2/submodules/qtwebengine-everywhere-src-6.10.2.tar.xz"
+  mirror "https://qt.mirror.constant.com/archive/qt/6.10/6.10.2/submodules/qtwebengine-everywhere-src-6.10.2.tar.xz"
+  mirror "https://mirrors.ukfast.co.uk/sites/qt.io/archive/qt/6.10/6.10.2/submodules/qtwebengine-everywhere-src-6.10.2.tar.xz"
+  sha256 "856eddf292a69a88618567deea67711b4ec720e69bcb575ed7bb539c9023961e"
   license all_of: [
     { any_of: ["LGPL-3.0-only", "GPL-2.0-only", "GPL-3.0-only"] },
     { "GPL-3.0-only" => { with: "Qt-GPL-exception-1.0" } }, # qwebengine_convert_dict; QtWebEngineProcess
@@ -63,7 +63,6 @@ class Qtwebengine < Formula
   uses_from_macos "gperf" => :build
 
   uses_from_macos "krb5" # dlopen-ed in http_auth_gssapi_posix.cc
-  uses_from_macos "zlib"
 
   on_macos do
     depends_on "qttools"
@@ -111,6 +110,7 @@ class Qtwebengine < Formula
     depends_on "snappy"
     depends_on "systemd"
     depends_on "webp"
+    depends_on "zlib-ng-compat"
   end
 
   pypi_packages package_name:   "",
