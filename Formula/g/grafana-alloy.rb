343cdeb039c8e5f704692c193cb48c3b92f5ba2b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-05 20:07:36 +0000

    grafana-alloy 1.13.0

--- Formula/g/grafana-alloy.rb
+++ Formula/g/grafana-alloy.rb
@@ -1,8 +1,8 @@
 class GrafanaAlloy < Formula
   desc "OpenTelemetry Collector distribution with programmable pipelines"
   homepage "https://grafana.com/oss/alloy-opentelemetry-collector/"
-  url "https://github.com/grafana/alloy/archive/refs/tags/v1.12.2.tar.gz"
-  sha256 "9e686f6d61b899b900f90ff796827cccde49b28cceb10cda9c4c0b1775613be7"
+  url "https://github.com/grafana/alloy/archive/refs/tags/v1.13.0.tar.gz"
+  sha256 "eae757e2bf67ba6ba45a0ca00e0c58d20fdddd8a17884201d6f3f87bb5686b1b"
   license "Apache-2.0"
   head "https://github.com/grafana/alloy.git", branch: "main"
 
