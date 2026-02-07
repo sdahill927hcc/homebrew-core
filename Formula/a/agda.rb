b70e40027ff1c694bb34b7e591041f772fd51130
Michael Cho <michael@michaelcho.dev>
2026-02-07 16:11:51 -0500

    agda: use `zlib-ng-compat` on Linux

--- Formula/a/agda.rb
+++ Formula/a/agda.rb
@@ -92,7 +92,10 @@ class Agda < Formula
 
   uses_from_macos "libffi"
   uses_from_macos "ncurses"
-  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     agda2hs_build = buildpath/"agda2hs"
